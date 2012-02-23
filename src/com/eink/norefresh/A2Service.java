784e17a4bcdf2672289763279a2ffe9c09a86513
Marcelo Póvoa <marspeoplester@gmail.com>
2012-02-22 23:49:17 -0200

    Use onTouchListener object instead of being a Service interface

--- src/com/eink/norefresh/A2Service.java
+++ src/com/eink/norefresh/A2Service.java
@@ -4,13 +4,12 @@ import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
-import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 
-public class A2Service extends Service implements OnTouchListener
+public class A2Service extends Service
 {
     View dummyView;
     private float lastY;
@@ -27,9 +26,42 @@ public class A2Service extends Service implements OnTouchListener
     @Override
     public void onCreate() {
         super.onCreate();
-        /* Invisible dummy view to receive touch events */
+        OnTouchListener touch = new OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                float x = event.getX();
+                float y = event.getY();
+
+                if (x > lastX && y > lastY)
+                    downHit++;
+                else
+                    downHit = 0;
+                
+                if (x < lastX && y < lastY)
+                    upHit++;
+                else
+                    upHit = 0;
+
+                lastX = x;
+                lastY = y;
+
+                if (downHit == HIT_COUNT_TARGET - 1) {
+                    N2EpdController.enterA2Mode();
+                    downHit = 0;
+                } else if (upHit == HIT_COUNT_TARGET - 1) {
+                    N2EpdController.exitA2Mode();
+                    N2EpdController.setGL16Mode(1);
+                    upHit = 0;
+                }
+                return false;
+            }
+        };
+        /*
+         * Invisible dummy view to receive touch events
+         */
         dummyView = new View(this);
-        dummyView.setOnTouchListener(this);
+        dummyView.setOnTouchListener(touch);
 
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
@@ -37,11 +69,9 @@ public class A2Service extends Service implements OnTouchListener
                 WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
                 WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
-        params.gravity = Gravity.RIGHT | Gravity.TOP;
-        params.setTitle("");
         WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
         wm.addView(dummyView, params);
-        
+
         downHit = upHit = 0;
     }
 
@@ -52,28 +82,5 @@ public class A2Service extends Service implements OnTouchListener
             ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(dummyView);
             dummyView = null;
         }
-    }
-    
-    public boolean onTouch(View v, MotionEvent event) {
-        float x = event.getX();
-        float y = event.getY();
-        
-        if (x > lastX && y> lastY) downHit++;
-        else downHit=0;
-        if (x < lastX && y < lastY) upHit++;
-        else upHit=0;
-        
-        lastX=x;
-        lastY=y;
-        
-        if (downHit == HIT_COUNT_TARGET - 1) {
-            N2EpdController.enterA2Mode();
-            downHit = 0;
-        } else if (upHit == HIT_COUNT_TARGET - 1) {
-            N2EpdController.exitA2Mode();
-            N2EpdController.setGL16Mode(1);
-            upHit = 0;
-        }
-        return false;
     }
 }
