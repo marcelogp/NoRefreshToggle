61b5f07206e2fa0c31c432f1f6fa59957ed69d11
Marcelo Póvoa <marspeoplester@gmail.com>
2012-03-04 20:53:24 -0300

    Avoid initial A2 ghosting by flashing a blank widget.Button on the entire screen.

--- src/com/eink/norefresh/A2Service.java
+++ src/com/eink/norefresh/A2Service.java
@@ -3,97 +3,110 @@ package com.eink.norefresh;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.os.CountDownTimer;
 import android.os.IBinder;
-import android.view.MotionEvent;
-import android.view.View;
+import android.view.*;
 import android.view.View.OnTouchListener;
-import android.view.WindowManager;
+import android.widget.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class A2Service extends Service
 {
-    View dummyView;
+    private View dummyView;
+    private Button blankButton;
     private float lastY;
     private float lastX;
     private int downHit;
     private int upHit;
     private static int HIT_COUNT_TARGET = 4;
-    private boolean justCreated = false;
+    private boolean ignoreNext = false;
     private Process process;
     private BufferedReader reader;
     private boolean a2Active = false;
-
+    private CountDownTimer timer;
+    private static boolean GESTURES_ENABLED = true;
+    private Display display;
+    
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
-
+    
     @Override
     public void onCreate() {
         super.onCreate();
-
+        
         try {
             process = Runtime.getRuntime().exec("/system/bin/logcat NATIVE-EPD:D EPD#NoRefreshToggle:D *:S");
             reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
+        
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        display = wm.getDefaultDisplay();
 
-        OnTouchListener touch = new OnTouchListener()
-        {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                float x = event.getX();
-                float y = event.getY();
-
-                if (x > lastX && y > lastY)
-                    downHit++;
-                else
-                    downHit = 0;
-
-                if (x < lastX && y < lastY)
-                    upHit++;
-                else
-                    upHit = 0;
-
-                lastX = x;
-                lastY = y;
-
-                if (downHit == HIT_COUNT_TARGET - 1) {
-                    setEpdA2();
-                    downHit = 0;
-                } else if (upHit == HIT_COUNT_TARGET - 1) {
-                    setEpdNormal();
-                    upHit = 0;
-                }
-                return false;
-            }
-        };
         /*
          * Invisible dummy view to receive touch events
          */
         dummyView = new View(this);
-        dummyView.setOnTouchListener(touch);
-
+        
+        blankButton = new Button(this);
+        blankButton.setVisibility(View.GONE);
+        
+        if (GESTURES_ENABLED) {
+            OnTouchListener touch = new OnTouchListener()
+            {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    float x = event.getX();
+                    float y = event.getY();
+                    
+                    if (x > lastX && y > lastY)
+                        downHit++;
+                    else
+                        downHit = 0;
+                    
+                    if (x < lastX && y < lastY)
+                        upHit++;
+                    else
+                        upHit = 0;
+                    
+                    lastX = x;
+                    lastY = y;
+                    
+                    if (downHit == HIT_COUNT_TARGET - 1) {
+                        setEpdA2();
+                        downHit = 0;
+                    } else if (upHit == HIT_COUNT_TARGET - 1) {
+                        setEpdNormal();
+                        upHit = 0;
+                    }
+                    return false;
+                }
+            };
+            dummyView.setOnTouchListener(touch);
+            ignoreNext = true;
+        } else
+            ignoreNext = false;
+        
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
                 WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
-        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
         wm.addView(dummyView, params);
-
-        justCreated = true;
-
+        wm.addView(blankButton, params);
+        
         downHit = upHit = 0;
     }
-
+    
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (!justCreated && !guessA2Active()) {
+        if (!ignoreNext && !guessA2Active()) {
             try {
                 /*
                  * Some delay to avoid setting A2 before Activity ends, which
@@ -104,20 +117,46 @@ public class A2Service extends Service
             }
             setEpdA2();
         }
-        justCreated = false;
-
+        ignoreNext = false;
+        
         return Service.START_STICKY;
     }
-
+    
     private void setEpdNormal() {
         N2EpdController.exitA2Mode();
         N2EpdController.setGL16Mode(1);
     }
-
+    
     private void setEpdA2() {
-        N2EpdController.enterA2Mode();
+        /*
+         * Screen should go temporarily blank
+         */
+        blankButton.setHeight(display.getHeight());
+        blankButton.setWidth(display.getWidth());
+        blankButton.setVisibility(View.VISIBLE);
+        blankButton.postInvalidate();
+
+        /*
+         * Set A2 mode after some delay to force an entire redraw in 1-bit
+         * (avoid initial ghosting)
+         */
+        timer = new CountDownTimer(250, 250)
+        {
+            @Override
+            public void onTick(long arg0) {
+            }
+            
+            @Override
+            public void onFinish() {
+                N2EpdController.enterA2Mode();
+                blankButton.setVisibility(View.GONE);
+            }
+        }.start();
     }
 
+    /*
+     * Tries to guess if A2 is active by checking last relevant entry at logcat
+     */
     private boolean guessA2Active() {
         try {
             String line;
@@ -132,13 +171,17 @@ public class A2Service extends Service
         }
         return a2Active;
     }
-
+    
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (dummyView != null) {
-            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(dummyView);
-            dummyView = null;
-        }
+        removeView(dummyView);
+        removeView(blankButton);
+        
+    }
+
+    private void removeView(View v) {
+        if (v != null)
+            ((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(v);
     }
 }
